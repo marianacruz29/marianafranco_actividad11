806a21dc180b5470b8aebf809504ef3f284bc9f7
marianacruz29 <mariana.cruzfr@gmail.com>
2020-12-10 00:03:37 -0600

    actividad11

---
+++ funciones.js
@@ -0,0 +1,27 @@
+const a=500;
+const b=300;
+const suma=a+b;
+alert ("El resultado de la suma es:"+suma);
+
+const x=30;
+const y=20;
+const resta=x-y;
+alert("El resultado de la resta es:"+resta);
+
+const mensaje = "La Voz De Tu Cabeza Siempre Tiene El Mismo Volumen";
+console.log(mensaje);
+console.log(mensaje.toUpperCase());
+console.log(mensaje.toLowerCase());
+
+const texto= "    No puedes hacerte cosquillas a ti mismo   ";
+console.log(texto);
+console.log(texto.trim());
+
+const frutas= "Fresa Mango Piña Uva Manzana";
+const arreglo = frutas.split(" ");
+console.log(frutas);
+console.log(arreglo);
+
+const email ="correo@gmail.com";
+console.log(email);
+console.log(email.split("@"));
